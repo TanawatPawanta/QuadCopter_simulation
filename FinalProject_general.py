5444d4212b9c08da87073d4a2cafb92f332f8b2b
Fzil0n <tanawat84598@gmail.com>
2023-12-06 22:35:15 +0700

    integrate dynamic and visualization

--- FinalProject_general.py
+++ FinalProject_general.py
@@ -18,8 +18,8 @@ Izz_val = 8.801*1e-3
 
 # Create an instance of QuadcopterDynamics
 quadcopter = QuadcopterDynamics(g_val, m_val, l_val, K_val, A_x_val, A_y_val, A_z_val, b_val, Ixx_val, Iyy_val, Izz_val)
-# quadcopter_visualization = Quadcopter3DVisualization()
-# quadcopter_controller = QuadcopterController(quadcopter_visualization)
+quadcopter_visualization = Quadcopter3DVisualization()
+quadcopter_controller = QuadcopterController(quadcopter_visualization)
 # [x y z ]
 # [phi theta psi]
 # [m1 m2 m3 m4]
@@ -27,7 +27,10 @@ om = 1.075
 spd = om*math.sqrt(1/K_val)
 print("spd : ",spd)
 dspd = 0.05*spd
-omega = [spd-(0.5*dspd), spd+(0.5*dspd), spd-(0.5*dspd), spd+(0.5*dspd)]
+# #spin about z-axis and move +z
+# omega = [spd-(0.5*dspd), spd+(0.5*dspd), spd-(0.5*dspd), spd+(0.5*dspd)]
+# #spin about x-axis
+omega = [spd, spd-(0.5*dspd), spd, spd]
 print("omega", omega)
 myTimer = time.time()
 print("Start time : ",myTimer)
@@ -37,12 +40,13 @@ quadcopter.set_dt(dt)
 clk = 0
 step = 0
 timeStamp = time.time() + dt
+deltas = np.zeros(6,)
 print("timeStamp : ", timeStamp)
 while True:
     if(time.time()-timeStamp >= 0):
         print("========trick=======")
-        # quadcopter_controller.update_quadcopter_and_plot(position=quadcopter.position,orientation=quadcopter.orientation)
-        if step >= 100:
+        quadcopter_controller.update_quadcopter_and_plot(deltas=deltas)
+        if step < 0:
             print("Start time : ",myTimer)
             print("Stop time : ", time.time())
             break
@@ -52,6 +56,6 @@ while True:
             quadcopter.calculate_A_matrix(phi_val=quadcopter.orientation[0], theta_val=quadcopter.orientation[1])
             quadcopter.calculate_B_matrix(omega=omega, orientation=quadcopter.orientation, angularVelocity=quadcopter.angularVelo, linearVelocity=quadcopter.linearVelo)
             acc = quadcopter.calculate_x_solution()
-            quadcopter.updateState()          
+            deltas = quadcopter.updateState()          
             quadcopter.dynamicDebugger()
         timeStamp = time.time() + dt    
--- QuadcopterDynamics.py
+++ QuadcopterDynamics.py
@@ -31,7 +31,18 @@ class QuadcopterDynamics:
         
         self.linearAcc = np.zeros(3,).astype(float)
         self.angularAcc = np.zeros(3,).astype(float)
-
+        
+    def calculate_A_matrix(self, phi_val:float, theta_val:float)->None:
+        self.matrixA = np.array([])
+        self.matrixA = np.array([
+            [self.m_val, 0, 0,               0,                                        0,                                                                            0],
+            [0, self.m_val, 0,               0,                                        0,                                                                            0],
+            [0, 0, self.m_val,               0,                                        0,                                                                            0],
+            [0, 0, 0,             self.Ixx_val,                                        0,                                                              -self.Ixx_val*np.sin(theta_val)],
+            [0, 0, 0,               0,    self.Iyy_val - self.Iyy_val*np.sin(phi_val)**2 + self.Izz_val*np.sin(phi_val)**2,                                     np.cos(phi_val)*np.cos(theta_val)*np.sin(phi_val)*(self.Iyy_val - self.Izz_val)],
+            [0, 0, 0, -self.Ixx_val*np.sin(theta_val), np.cos(phi_val)*np.cos(theta_val)*np.sin(phi_val)*(self.Iyy_val - self.Izz_val), self.Izz_val*np.cos(phi_val)**2*np.cos(theta_val)**2 + self.Iyy_val*np.cos(theta_val)**2*np.sin(phi_val)**2 + self.Ixx_val*np.sin(theta_val)**2]
+        ])
+        
     def calculate_B_matrix(self,omega:list[float], orientation:list[float], angularVelocity:list[float], linearVelocity:list[float])->None:
         omega1_val = omega[0]
         omega2_val = omega[1]
@@ -56,16 +67,6 @@ class QuadcopterDynamics:
             [self.b_val*omega1_val**2 - self.b_val*omega2_val**2 + self.b_val*omega3_val**2 - self.b_val*omega4_val**2 + self.Ixx_val*phi_dot_val*theta_dot_val*np.cos(theta_val) + self.Iyy_val*phi_dot_val*theta_dot_val*np.cos(theta_val) - self.Izz_val*phi_dot_val*theta_dot_val*np.cos(theta_val) - self.Ixx_val*psi_dot_val*theta_dot_val*np.sin(2*theta_val) + self.Iyy_val*psi_dot_val*theta_dot_val*np.sin(2*theta_val) + self.Iyy_val*theta_dot_val**2*np.cos(phi_val)*np.sin(phi_val)*np.sin(theta_val) - self.Izz_val*theta_dot_val**2*np.cos(phi_val)*np.sin(phi_val)*np.sin(theta_val) - 2*self.Iyy_val*phi_dot_val*theta_dot_val*np.cos(phi_val)**2*np.cos(theta_val) + 2*self.Izz_val*phi_dot_val*theta_dot_val*np.cos(phi_val)**2*np.cos(theta_val) - 2*self.Iyy_val*phi_dot_val*psi_dot_val*np.cos(phi_val)*np.cos(theta_val)**2*np.sin(phi_val) + 2*self.Izz_val*phi_dot_val*psi_dot_val*np.cos(phi_val)*np.cos(theta_val)**2*np.sin(phi_val) - 2*self.Iyy_val*psi_dot_val*theta_dot_val*np.cos(phi_val)**2*np.cos(theta_val)*np.sin(theta_val) + 2*self.Izz_val*psi_dot_val*theta_dot_val*np.cos(phi_val)**2*np.cos(theta_val)*np.sin(theta_val)]
         ])
 
-    def calculate_A_matrix(self, phi_val:float, theta_val:float)->None:
-        self.matrixA = np.array([])
-        self.matrixA = np.array([
-            [self.m_val, 0, 0,               0,                                        0,                                                                            0],
-            [0, self.m_val, 0,               0,                                        0,                                                                            0],
-            [0, 0, self.m_val,               0,                                        0,                                                                            0],
-            [0, 0, 0,             self.Ixx_val,                                        0,                                                              -self.Ixx_val*np.sin(theta_val)],
-            [0, 0, 0,               0,    self.Iyy_val - self.Iyy_val*np.sin(phi_val)**2 + self.Izz_val*np.sin(phi_val)**2,                                     np.cos(phi_val)*np.cos(theta_val)*np.sin(phi_val)*(self.Iyy_val - self.Izz_val)],
-            [0, 0, 0, -self.Ixx_val*np.sin(theta_val), np.cos(phi_val)*np.cos(theta_val)*np.sin(phi_val)*(self.Iyy_val - self.Izz_val), self.Izz_val*np.cos(phi_val)**2*np.cos(theta_val)**2 + self.Iyy_val*np.cos(theta_val)**2*np.sin(phi_val)**2 + self.Ixx_val*np.sin(theta_val)**2]
-        ])
 
     def calculate_x_solution(self):
         x = np.linalg.solve(self.matrixA, self.matrixB)
@@ -97,18 +98,22 @@ class QuadcopterDynamics:
     
     def updateState(self):
         print("Updating...")
+        deltasPos = np.zeros(3,)
+        deltasOrien = np.zeros(3,)
         for i in range(3):
+            # update current state
             self.linearVelo[i] = self.linearVelo_minus[i] + (self.linearAcc[i]*self.dt)
-            self.angularVelo[i] = float(self.angularVelo_minus[i] + self.angularAcc[i]*self.dt)
-
+            self.angularVelo[i] = self.angularVelo_minus[i] + self.angularAcc[i]*self.dt
             self.position[i] = self.position_minus[i] + self.linearVelo[i]*self.dt + 0.5*self.linearAcc[i]*self.dt*self.dt
             self.orientation[i] = self.orientation_minus[i] + self.angularVelo[i]*self.dt + 0.5*self.angularAcc[i]*self.dt*self.dt
-            
+            deltasPos[i] = self.linearVelo[i]*self.dt + 0.5*self.linearAcc[i]*self.dt*self.dt
+            deltasOrien[i] = self.angularVelo[i]*self.dt + 0.5*self.angularAcc[i]*self.dt*self.dt
+            # update n-1 state
             self.linearVelo_minus[i] = self.linearVelo[i]
             self.angularVelo_minus[i] = self.angularVelo[i]
             self.position_minus[i] = self.position[i]
             self.orientation_minus[i] = self.orientation[i]
-
+        return np.concatenate((deltasOrien,deltasPos),axis=0)
     
     def dynamicDebugger(self):
         print("Angular prpoties")
